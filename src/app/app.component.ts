8c9a5791ef3f3a1c912375ca1d7fd28b02b6fdd9
Weston Woodard <weston.woodard@slalom.com>
2022-02-28 16:45:17 -0600

    home component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+// this is called a decorator. we know it is a decorator because of the @ symbol
 @Component({
+  // how your component is referenced in your html files
   selector: 'app-root',
+  // the html file that this component should utilize, called the view
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // the styles file(s) that this componet should utilize
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'net-ninja-app';
