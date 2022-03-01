1b09dbbd1e4c7e9f625534b64c4241a26d43c0cc
Weston Woodard <weston.woodard@slalom.com>
2022-03-01 13:56:55 -0600

    practice complete

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
-  styleUrls: ['./directory.component.css']
+  styleUrls: ['./directory.component.css'],
 })
 export class DirectoryComponent implements OnInit {
+  ninja: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) {
+    // grabs the route param to make unique route
+    this.ninja = route.snapshot.params['ninja'];
   }
 
+  ngOnInit(): void {}
 }
